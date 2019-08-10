407e6e99385677586b6fc4a61a4497653803d1f4
Eesa <essaji44@gmail.com>
2019-08-11 03:44:35 +0400

    Newrelic - pass newrelic as argument in extension

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-## TODO
+# Apollo New Relic Extension Plus
+Creates newrelic transaction for graphql queries.
+
+This is a forked repository of `apollo-newrelic-extension`. We created this plus package because
+the previous package uses `operationName` as the `transactionName` which is an optional
+ attribute in a graphql request and can be `null` at times. 
+
+This package always uses graphql query name as the name of the transaction.
+
+## Usage
+```javascript
+const newrelic = require('newrelic')
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+const ApolloNewrelicExtensionPlus = require('apollo-newrelic-extension')
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // ...additional configuration...
+
+  // Thunk for creating the newrelic extension
+  extensions: [() => new ApolloNewrelicExtensionPlus(newrelic)],
+
+  // Be sure to enable tracing
+  tracing: true
+})
+
+const app = express()
+server.applyMiddleware({ app })
+
+app.listen(3000, () => console.log('Server listening on port 3000'))
+```
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 const R = require('ramda')
 const { GraphQLExtension } = require('graphql-extensions/dist/index')
 const gql = require('graphql-tag')
-const newrelic = require('newrelic')
 const fieldTraceSummary = require('./field-trace-summary')
 
 const errorCount = R.pipe(R.propOr([], 'errors'), R.length)
 
 class NewRelicExtensionPlus extends GraphQLExtension {
+    constructor(newrelic) {
+        super()
+        this.newrelic = newrelic
+    }
     requestDidStart ({ queryString, variables, persistedQueryHit }) {
         const queryObject = gql`${queryString}`
         const transactionName = queryObject.definitions[0].selectionSet.selections
             .reduce((init, q, idx) => idx === 0 ? `${q.name.value}` : `${init}, ${q.name.value}` , '')
-        newrelic.setTransactionName(`graphql (${transactionName})`)
-        newrelic.addCustomAttribute('gqlQuery', queryString)
-        newrelic.addCustomAttribute('gqlVars', JSON.stringify(variables))
-        newrelic.addCustomAttribute('persistedQueryHit', persistedQueryHit)
+        this.newrelic.setTransactionName(`graphql (${transactionName})`)
+        this.newrelic.addCustomAttribute('gqlQuery', queryString)
+        this.newrelic.addCustomAttribute('gqlVars', JSON.stringify(variables))
+        this.newrelic.addCustomAttribute('persistedQueryHit', persistedQueryHit)
     }
 
     willSendResponse ({ graphqlResponse }) {
@@ -22,8 +25,8 @@ class NewRelicExtensionPlus extends GraphQLExtension {
             R.pathOr([], ['extensions', 'tracing']),
             fieldTraceSummary
         )(graphqlResponse)
-        newrelic.addCustomAttribute('traceSummary', tracingSummary)
-        newrelic.addCustomAttribute('errorCount', errorCount(graphqlResponse))
+        this.newrelic.addCustomAttribute('traceSummary', tracingSummary)
+        this.newrelic.addCustomAttribute('errorCount', errorCount(graphqlResponse))
     }
 }
 
